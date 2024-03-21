21960a827f47adff33d8ef62728e0dab3bf63fdc
Faith-Jackson <undisputedfaith@gmail.com>
2024-03-21 01:16:35 +0100

    done with all tasks

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
-def weight_average(my_list=[]):
-    if not my_list:
-        return 0
-    average = 0
-    div = 0
-    for tup in my_list:
-        average += tup[0] * tup[1]
-        div += tup[1]
-    return float(average / div)
+# -----------------------------------------------------------
+# Python program that:
+# demonstrates how to compute the square value of all integers of a matrix
+# using map
+#
+# (C) 2022 Igbinijesu Samuel, Lagos, Nigeria
+# email igbinijesusamuel@gmail.com
+# -----------------------------------------------------------
 
+
+def square_matrix_map(matrix=[]):
+    return list(map(lambda x: list(map(lambda i: i ** 2, x)), matrix))
