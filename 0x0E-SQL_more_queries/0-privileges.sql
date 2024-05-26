d3d4950172b567dd19fff2d5407e4fe6a62729f8
Faith-Jackson <undisputedfaith@gmail.com>
2024-05-26 01:27:56 +0100

    done

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,25 @@
--- Lists all privileges of the MySQL users user_0d_1 and user_0d_2
-SHOW GRANTS FOR 'user_0d_1'@'localhost';
-SHOW GRANTS FOR 'user_0d_2'@'localhost';
+#!/bin/bash
+
+# MySQL root credentials
+MYSQL_USER="root"
+MYSQL_PASSWORD="yourpassword"
+
+# Users to check
+USERS=("user_0d_1" "user_0d_2")
+
+# Function to get privileges for a user
+get_user_privileges() {
+    local user=$1
+    echo "Privileges for $user:"
+    mysql -u"$MYSQL_USER" -p"$MYSQL_PASSWORD" -e "SHOW GRANTS FOR '$user'@'localhost';" 2>/dev/null
+    if [ $? -ne 0 ]; then
+        echo "Error retrieving privileges for $user"
+    fi
+    echo
+}
+
+# Main script
+for user in "${USERS[@]}"; do
+    get_user_privileges "$user"
+done
+
